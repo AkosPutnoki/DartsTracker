e98ecee66a183c888f8330513a9c05097c2ffa1f
Akos Putnoki <putnokiakos@gmail.com>
2018-04-17 16:00:44 +0200

    header and button designs added

--- Dartstracker2/app/src/main/java/com/example/jeno/dartstracker/Activites/StartActivity.java
+++ Dartstracker2/app/src/main/java/com/example/jeno/dartstracker/Activites/StartActivity.java
@@ -1,9 +1,9 @@
 package com.example.jeno.dartstracker.Activites;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +13,7 @@ import android.widget.RadioGroup;
 import com.example.jeno.dartstracker.Activites.MainActivity;
 import com.example.jeno.dartstracker.R;
 
-public class StartActivity extends Activity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
